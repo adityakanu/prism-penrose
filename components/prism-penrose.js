cd3405ca718d0889916c2dadcc67d0d851ff3300
Aditya Kanu <adityakanu1st@gmail.com>
2024-01-22 14:43:00 +0530

    removed style keywords

--- components/prism-penrose.js
+++ components/prism-penrose.js
@@ -3,14 +3,10 @@ Prism.languages.penrose = {
 		pattern: /"(?:[^\n\\"]|\\["\\ntbfr])*"/,
 		greedy: true
 	},
-	'comment': {
-		pattern: /(?:\/\*[\s\S]*?\*\/|--.*)/
-	},
+	'comment': /(?:\/\*[\s\S]*?\*\/|--.*)/,
 	'keyword': /\b(?:All|AutoLabel|Label|Let|NoLabel|for|in|where)\b/,
-	'builtin': /\b(?:above|as|below|collect|delete|encourage|ensure|except|false|forall|foreach|from|has|in|layer|listof|nameof|numberof|override|repeatable|then|true|where|with)\b/,
 	'operator': /:=|>=|<=|>|<|==|\\|&&|!|\+|\*|-|%|\/|mod|=/,
-	'function': /\$.*?\$/,
-	'class-name': /\b(?:bool|color|constraint|file|function|int|list|mat2x2|mat3x3|mat4x4|objective|path|scalar|shape|string|style|vec2|vec3|vec4)\b/i,
+	'function': /\$.*\$/,
 	'number': {
 		pattern: /\b[+-]?(?:\d+(?:\.\d*)?(?:[eE][+-]?\d+)?|\.\d+(?:[eE][+-]?\d+)?)\b/,
 		lookbehind: true
